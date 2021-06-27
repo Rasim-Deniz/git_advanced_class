de5444087d1b822069635e9fb775eec2381d031a
Rasim-Deniz <rasimdeniz1996@gmail.com>
2021-06-27 15:49:57 +0200

    Added a function and logging it out

--- helloWorld.js
+++ helloWorld.js
@@ -1,7 +1,12 @@
-const sayGreeting = function() {
-  return 'Hello world!';
+const sayGreeting = function () {
+  return "Hello world!";
+};
+
+const sayBye = function () {
+  return "Bye!";
 };
 
 console.log(sayGreeting());
+console.log(sayBye());
 
 console.log("Hello again");
