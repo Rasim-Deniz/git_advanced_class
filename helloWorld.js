220d0da5bcbc7816c1a52cddb52c54f1a2c4b313
name <maha.karzoun@gmail.com>
2021-06-27 15:44:04 +0200

    add greeting

--- helloWorld.js
+++ helloWorld.js
@@ -1,7 +1,8 @@
-const sayGreeting = function() {
-  return 'Hello world!';
+const sayGreeting = function () {
+  return "Hello world!";
 };
 
 console.log(sayGreeting());
 
 console.log("Hello again");
+console.log("Hello from the other side (jylland) hahah");
